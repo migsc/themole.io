6d16a51fe3c4ab3045e39544037474cd5fae5291
Miguel Chateloin <miguel@chateloin.com>
2023-03-06 03:38:16 -0600

    fix type errors and seed

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,16 +1,19 @@
-import { type Dispatch, type SetStateAction, useEffect, useState } from 'react';
+import { type Dispatch, type SetStateAction, useEffect, useState } from "react";
 
-export default function useLocalStorage<T>(key: string, fallbackValue: T): [T, Dispatch<SetStateAction<T>>] {
-    const [value, setValue] = useState<T>(fallbackValue);
+export default function useLocalStorage<T>(
+  key: string,
+  fallbackValue: T
+): [T, Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(fallbackValue);
 
-    useEffect(() => {
-        const valueStored = localStorage.getItem(key) as string;
-        setValue(valueStored ? JSON.parse(valueStored) as T : fallbackValue );
-    }, [fallbackValue, key]);
+  useEffect(() => {
+    const valueStored: string = localStorage.getItem(key);
+    setValue(valueStored ? (JSON.parse(valueStored) as T) : fallbackValue);
+  }, [fallbackValue, key]);
 
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value));
-    }, [key, value]);
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
 
-    return [value, setValue];
+  return [value, setValue];
 }
