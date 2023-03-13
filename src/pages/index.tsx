a6dec00058916d4883f4b8b134cb1bf67801685c
Miguel Chateloin <miguel@chateloin.com>
2023-03-12 22:21:17 -0500

    fix config again 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,13 +30,13 @@ export async function getStaticProps(context) {
     };
   }
 
-  console.log(process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID);
-  console.log(process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY);
-  console.log(process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME);
-  console.log(process.env.NEXT_PUBLIC_AWS_REGION);
+  console.log(process.env.AWS_ACCESS_KEY_ID);
+  console.log(process.env.AWS_SECRET_ACCESS_KEY);
+  console.log(process.env.AWS_S3_BUCKET_NAME);
+  console.log(process.env.AWS_REGION);
 
   const pictureUrl = await s3.getSignedUrlPromise("getObject", {
-    Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
     Key: picture.filename,
     Expires: 60, // The expiration time of the signed URL in seconds
   });
